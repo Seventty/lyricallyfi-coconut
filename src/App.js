e133a6a35f17c3b0b536607e132d80a9d7361b0c
seventty <elmastigeron2015@gmail.com>
2021-05-31 13:59:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+import Header from './components/Header';
 
-function App() {
+const useStyles = makeStyles((theme)=> ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage:`url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: '5% 50%',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={classes.root}>
+        <CssBaseline/>
+        <Header/>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
